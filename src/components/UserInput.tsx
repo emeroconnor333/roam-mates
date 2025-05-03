e3896d7a1798454c30f9c526f5bac22e3d84ecea
emeroconnor333 <emeroconnor333@gmail.com>
2025-05-03 19:40:22 +0200

    fix

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -20,7 +20,7 @@ const UserInput: React.FC<Props> = ({ onChange }) => {
       <input
         type="text"
         onChange={handleChange}
-        placeholder="e.g. user_abc123"
+        placeholder="e.g. Ryan"
         className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-holiday-red"
       />
     </div>
